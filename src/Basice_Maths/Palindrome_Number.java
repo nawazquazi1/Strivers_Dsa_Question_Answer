16dbe60c97fcf5e5c12e676289a5d6978174db58
nawazquazi1 <nawazquazi356@gmail.com>
2024-05-27 16:29:09 +0530

    reverse a number

---
+++ src/Basice_Maths/Palindrome_Number.java
@@ -0,0 +1,8 @@
+package Basice_Maths;
+
+public class Palindrome_Number {
+
+    public static void Palindrome(int  n){
+
+    }
+}
--- src/Basice_Maths/Reverse_A_Number.java
+++ src/Basice_Maths/Reverse_A_Number.java
@@ -1,4 +1,18 @@
 package Basice_Maths;
 
 public class Reverse_A_Number {
+
+    public static int reverse(int x) {
+        int revers = 0;
+        while (x != 0) {
+            int ld = x % 10;
+            revers = (revers * 10) + ld;
+            x /= 10;
+        }
+        return revers;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(3243));
+    }
 }
