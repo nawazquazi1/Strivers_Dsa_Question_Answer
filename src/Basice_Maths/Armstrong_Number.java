603855252b2242455325eb7350ec1e345721f231
nawazquazi1 <nawazquazi356@gmail.com>
2024-05-27 17:12:46 +0530

    Palindrome_Number

---
+++ src/Basice_Maths/Armstrong_Number.java
@@ -0,0 +1,8 @@
+package Basice_Maths;
+
+public class Armstrong_Number {
+
+    public static boolean isArmstrong(int num) {
+        
+    }
+}
--- src/Basice_Maths/Palindrome_Number.java
+++ src/Basice_Maths/Palindrome_Number.java
@@ -2,7 +2,32 @@ package Basice_Maths;
 
 public class Palindrome_Number {
 
-    public static void Palindrome(int  n){
+    public static boolean Palindrome(int n) {
+        int revers = 0;
+        int duplicate = n;
+        while (n > 0) {
+            int ld = n % 10;
+            revers = (revers * 10) + ld;
+            n /= 10;
+        }
+        return revers == duplicate;
+    }
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x); // Convert to String
+        int n = s.length(); // Store the String length to int n
 
+        for (int i=0; i<n/2; i++) {
+            // We check whether the elements at the same distance from
+            // beginning and from ending are same, if not we return false
+            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        }
+
+        // if no flaws are found we return true
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome(-121));
     }
 }
