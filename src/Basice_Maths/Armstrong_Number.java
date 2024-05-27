2be223cca77b9dcce26f0e3e52870682af05c6e6
nawazquazi1 <nawazquazi356@gmail.com>
2024-05-27 17:18:10 +0530

    Armstrong Number

--- src/Basice_Maths/Armstrong_Number.java
+++ src/Basice_Maths/Armstrong_Number.java
@@ -3,6 +3,35 @@ package Basice_Maths;
 public class Armstrong_Number {
 
     public static boolean isArmstrong(int num) {
+        // Calculate the number of
+        // digits in the given number
+        int k = String.valueOf(num).length();
+        // Initialize the sum of digits
+        // raised to the power of k to 0
+        int sum = 0;
+        // Copy the value of the input
+        // number to a temporary variable n
+        int n = num;
+        // Iterate through each
+        // digit of the number
+        while (n > 0) {
+            // Extract the last
+            // digit of the number
+            int ld = n % 10;
+            // Add the digit raised to
+            // the power of k to the sum
+            sum += (int) Math.pow(ld, k);
+            // Remove the last digit
+            // from the number
+            n = n / 10;
+        }
+        // Check if the sum of digits raised to
+        // the power of k equals the original number
+        return sum == num;
+    }
 
+    public static void main(String[] args) {
+
+        System.out.println(isArmstrong(1531));
     }
 }
