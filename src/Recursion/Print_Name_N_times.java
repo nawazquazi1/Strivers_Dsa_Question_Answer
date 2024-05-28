f8585e17398c56b843a7517634edc214465b3d0d
nawazquazi1 <nawazquazi356@gmail.com>
2024-05-28 15:17:55 +0530

    Print_Name_N_times

---
+++ src/Recursion/Print_Name_N_times.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Print_Name_N_times {
+    static void func(int i, int n){
+
+        // Base Condition.
+        if(i>n) return;
+        System.out.println("Raj");
+
+        // Function call to print till i increments.
+        func(i+1,n);
+
+    }
+    public static void main(String[] args) {
+
+        // Here, let’s take the value of n to be 4.
+        int n = 4;
+        func(1,n);
+    }
+}
