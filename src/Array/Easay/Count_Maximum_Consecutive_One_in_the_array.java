b3ef0508ef32615c9578680dfac63393cb44d618
nawazquazi1 <nawazquazi356@gmail.com>
2024-07-01 19:31:38 +0530

    Count Maximum Consecutive One in the array

--- src/Array/Easay/Count_Maximum_Consecutive_One_in_the_array.java
+++ src/Array/Easay/Count_Maximum_Consecutive_One_in_the_array.java
@@ -1,4 +1,24 @@
 package Array.Easay;
 
 public class Count_Maximum_Consecutive_One_in_the_array {
+
+    public static  int findMaxConsecutive(int[] arr) {
+        int max=0;
+        int count=0;
+        for (int j : arr) {
+            if (j == 1) {
+                count++;
+            }else {
+                count = 0;
+            }
+            max=Math.max(max,count);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int []arr={0,0,1,0,1,1,1,0,0,1,0,1,0,0,1};
+        System.out.println(findMaxConsecutive(arr));
+    }
 }
